cb66fe3e050445923851630e29233ed2087f1f06
WoonHaKim <work@woonakim.info>
2025-02-01 19:54:40 +0900

    Fix iOS build

--- ios/AmazonIvsManager.m
+++ ios/AmazonIvsManager.m
@@ -1,5 +1,5 @@
 #import "React/RCTBridgeModule.h"
-#import "RCTViewManager.h"
+#import "React/RCTViewManager.h"
 
 @interface RCT_EXTERN_MODULE(AmazonIvsManager, RCTViewManager)
 RCT_EXPORT_VIEW_PROPERTY(resizeMode, NSString)
